d20ed46dc23f43c75682d3b752f62b9d84b75540
DataWarelarrylu <lsoralusheng1980@sina.com>
2018-04-23 14:34:32 +0800

    加上css

--- BeautifulSoup学习.py
+++ BeautifulSoup学习.py
@@ -409,3 +409,108 @@ for i, child in enumerate(soup.p.descendants):
         # find_all_previous()
         # 返回节点后所有符合条件的节点, find_previous()
         # 返回第一个符合条件的节点
+
+# CSS选择器
+# 通过select()直接传入CSS选择器即可完成选择
+html='''
+<div class="panel">
+    <div class="panel-heading">
+        <h4>Hello</h4>
+    </div>
+    <div class="panel-body">
+        <ul class="list" id="list-1">
+            <li class="element">Foo</li>
+            <li class="element">Bar</li>
+            <li class="element">Jay</li>
+        </ul>
+        <ul class="list list-small" id="list-2">
+            <li class="element">Foo</li>
+            <li class="element">Bar</li>
+        </ul>
+    </div>
+</div>
+'''
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html, 'lxml')
+print(soup.select('.panel .panel-heading'))
+print(soup.select('ul li'))
+print(soup.select('#list-2 .element'))
+print(type(soup.select('ul')[0]))
+# =======================================
+html='''
+<div class="panel">
+    <div class="panel-heading">
+        <h4>Hello</h4>
+    </div>
+    <div class="panel-body">
+        <ul class="list" id="list-1">
+            <li class="element">Foo</li>
+            <li class="element">Bar</li>
+            <li class="element">Jay</li>
+        </ul>
+        <ul class="list list-small" id="list-2">
+            <li class="element">Foo</li>
+            <li class="element">Bar</li>
+        </ul>
+    </div>
+</div>
+'''
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html, 'lxml')
+for ul in soup.select('ul'):
+    print(ul.select('li'))
+# =============================================
+# 获取属性
+html='''
+<div class="panel">
+    <div class="panel-heading">
+        <h4>Hello</h4>
+    </div>
+    <div class="panel-body">
+        <ul class="list" id="list-1">
+            <li class="element">Foo</li>
+            <li class="element">Bar</li>
+            <li class="element">Jay</li>
+        </ul>
+        <ul class="list list-small" id="list-2">
+            <li class="element">Foo</li>
+            <li class="element">Bar</li>
+        </ul>
+    </div>
+</div>
+'''
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html, 'lxml')
+for ul in soup.select('ul'):
+    print(ul['id'])
+    print(ul.attrs['id'])
+# ===========================================
+html='''
+<div class="panel">
+    <div class="panel-heading">
+        <h4>Hello</h4>
+    </div>
+    <div class="panel-body">
+        <ul class="list" id="list-1">
+            <li class="element">Foo</li>
+            <li class="element">Bar</li>
+            <li class="element">Jay</li>
+        </ul>
+        <ul class="list list-small" id="list-2">
+            <li class="element">Foo</li>
+            <li class="element">Bar</li>
+        </ul>
+    </div>
+</div>
+'''
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html, 'lxml')
+for li in soup.select('li'):
+    print(li.get_text())
+#     ==============================================
+# 总结
+# 推荐使用lxml解析库，必要时使用html.parser
+# 标签选择筛选功能弱但是速度快
+# 建议使用find()、find_all() 查询匹配单个结果或者多个结果
+# 如果对CSS选择器熟悉建议使用select()
+# 记住常用的获取属性和文本值的方法
